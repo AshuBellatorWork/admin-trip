62b2fae913b26c7c08119b14d8f0a5c00439444a
aditya-bellator <ashutosh@bellatorgroup.in>
2024-11-13 10:09:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default {
-  server: {
-    host: "0.0.0.0", // Expose the server to external network
-    port: 3001, // You can change the port if needed
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+});
+// export default {
+//   server: {
+//     host: "0.0.0.0", // Expose the server to external network
+//     port: 3001, // You can change the port if needed
+//   },
+// };
