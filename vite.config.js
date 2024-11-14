7f6141131ef1ac3d3c1299fdb06a3b100807072f
aditya-bellator <ashutosh@bellatorgroup.in>
2024-11-14 17:24:53 +0530

    create a new build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+// export default defineConfig({
+//   plugins: [react()],
+// });
 // export default {
 //   server: {
 //     host: "0.0.0.0", // Expose the server to external network
 //     port: 3001, // You can change the port if needed
 //   },
 // };
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Vite config documentation: https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3001, // You can change the port if needed, default is 3000
+  },
+});
